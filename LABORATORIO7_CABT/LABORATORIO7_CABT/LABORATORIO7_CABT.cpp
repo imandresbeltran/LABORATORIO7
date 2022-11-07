564b20d8b3aa1be68aa22d83f900a6df90f3084a
imandresbeltran <cabeltran@correo.url.edu.gt>
2022-11-07 08:11:45 -0600

    ACTUALIZACIÓN PARA VALIDAR LA CORRECTA CORRECCIÓN DE ERRORES DE COMPILACIÓN

--- LABORATORIO7_CABT/LABORATORIO7_CABT/LABORATORIO7_CABT.cpp
+++ LABORATORIO7_CABT/LABORATORIO7_CABT/LABORATORIO7_CABT.cpp
@@ -40,11 +40,13 @@ int main()
 		switch (option)
 		{
 		case 1:
+			ordenamiento s;
 			n = sizeof(numeros[0]) / sizeof(numeros[0]);
+
 			//ordenamiento ordenar;
-			quickSort(numeros, 0, n - 1);
+			s.quickSort(numeros, 0, n - 1);
 			cout << "------ A R R E G L O  O R D E N A D O ------" << "\r\n";
-			printArray(numeros, n);
+			s.printArray(numeros, n);
 			break;
 		case 2:
 			cout << "HASTA PRONTO!" << "\r\n";
@@ -59,60 +61,7 @@ int main()
 		}
 	} while (option != 2);
 };
-	void swap(int* a, int* b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-
-	  /* This function takes last element as pivot, places
-  the pivot element at its correct position in sorted
-  array, and places all smaller (smaller than pivot)
-  to left of pivot and all greater elements to right
-  of pivot */
- int partition(int arr[], int low, int high)
-{
-	int pivot = arr[high]; // pivot 
-	int i = (low - 1); // Index of smaller element and indicates
-	// the right position of pivot found so far
-
-	for (int j = low; j <= high - 1; j++) {
-		// If current element is smaller than the pivot
-		if (arr[j] < pivot) {
-			i++; // increment index of smaller element
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
-
-	  /* The main function that implements QuickSort
-  arr[] --> Array to be sorted,
-  low --> Starting index,
-  high --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-	if (low < high) {
-		/* pi is partitioning index, arr[p] is now
-		at right place */
-		int pi = partition(arr, low, high);
-
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
-}
-	  /* Function to print an array */
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
+	
 
 
 
